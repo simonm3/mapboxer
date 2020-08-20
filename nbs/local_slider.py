de6958cbb44d456520219fbd31783dbadf3f0db5
simonm3 <simonm3@gmail.com>
2020-08-20 17:11:42 +0100

    readd nbs

---
+++ nbs/local_slider.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from ipstartup import *
+import geopandas as gpd
+import plotly.express as px
+from mapboxer.elections import get, clean, show
+from mapboxer.utils import iframe
+from mapboxer import Twomaps
+
+const, constcentres = clean.ge(2010)
+
+year1=2011
+wards, wardcentres = clean.local(year1)
+m1 = show.get_map(wards, wardcentres, const, constcentres, "party")
+
+year2=2015
+wards, wardcentres = clean.local(year2)
+m2 = show.get_map(wards, wardcentres, const, constcentres, "party")
+
+m1.title = f"Local elections {year2} (left of slider is {year1})"
+s = Twomaps(m1, m2, "slider")
+iframe(s.html())
+
+s.save("local_slider2015")
+
+
